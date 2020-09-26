a26834374b4e438ee6e1815052d4f8ab85a709db
Aneta <aneta.d93@gmail.com>
2020-09-27 01:49:43 +0200

    extracted cookie const

--- unibase_api.js
+++ unibase_api.js
@@ -1,6 +1,8 @@
 const fetch = require('node-fetch');
 const parseHtml = require('node-html-parser').parse
 
+const cookie = '_ga=GA1.2.745731147.1572898705; lan=pl; _ym_d=1590059062; _ym_uid=1590059062935480695; _gcl_au=1.1.1701424439.1598818857; uid=1914; _gid=GA1.2.127812211.1601062632; logged_in=oNNhFcMlkyj8Nijg';
+
 async function sendRequest(method, endpoint, body) {
     const url = "https://r.unibase.pl/" + endpoint;
 
@@ -17,7 +19,7 @@ async function sendRequest(method, endpoint, body) {
             "sec-fetch-site": "same-origin",
             "sec-fetch-user": "?1",
             "upgrade-insecure-requests": "1",
-            'Cookie': '_ga=GA1.2.745731147.1572898705; lan=pl; _ym_d=1590059062; _ym_uid=1590059062935480695; _gcl_au=1.1.1701424439.1598818857; uid=1914; _gid=GA1.2.127812211.1601062632; logged_in=oNNhFcMlkyj8Nijg',
+            'Cookie': cookie,
             "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36",
         },
         "referrer": url,
